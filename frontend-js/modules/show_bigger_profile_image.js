59886f5933cb60536fe5f565c1a1e0f5974b6678
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2022-09-12 07:28:21 -0500

    native tailwind and new modal

--- frontend-js/modules/show_bigger_profile_image.js
+++ frontend-js/modules/show_bigger_profile_image.js
@@ -1,33 +1,24 @@
 export default class ToggleImage {
-    constructor(){
-        this.profileImage = document.querySelector("#profile-image");
-        this.closeImage = document.querySelector("#close-image");
-        this.profileImageModal = document.querySelector("#profile-image-modal");
-        this.body = document.querySelector("body");
-        this.events();
-    }
-    // EVENTS
-    events(){
-        this.profileImage.addEventListener("click", (e) => this.profileImageHandler(e));
-        this.closeImage.addEventListener("click", () => this.closeImageHandler());
-        this.body.addEventListener("click", () => this.closeImageHandler())
-    }
+  constructor() {
+    this.profileImage = document.querySelector('#profile-image');
+    this.closeImage = document.querySelector('#close-image');
+    this.profileImageModal = document.querySelector('#profile-image-modal');
+    this.events();
+  }
+  // EVENTS
+  events() {
+    this.profileImage.addEventListener('click', () => this.profileImageHandler());
+    this.closeImage.addEventListener('click', () => this.closeImageHandler());
+  }
 
-    // METHODS
-    profileImageHandler(e){
-        e.stopPropagation();
-        if(this.profileImageModal.style.display == "none"){
-            this.profileImageModal.style.display = "block"
-            this.body.style.backgroundColor = "black"
-        } else {
-            this.profileImageModal.style.display = "none"
-            this.body.style.backgroundColor = "#edf2f7"
-        }
-    }
-    
-    closeImageHandler(){
-        this.profileImageModal.style.display = "none"
-        this.body.style.backgroundColor = "#edf2f7"
-    }
-    
+  // METHODS
+  profileImageHandler() {
+    console.log('aaayy');
+    this.profileImageModal.classList.toggle('hidden');
+  }
+
+  closeImageHandler() {
+    console.log('close');
+    this.profileImageModal.classList.toggle('hidden');
+  }
 }
