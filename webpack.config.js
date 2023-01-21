d7b4e81584ed847b70e2c1a9371d6597f2593824
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2023-01-21 15:25:51 -0600

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,49 @@
-const path = require("path");
+const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./javascript/main.js",
+  entry: './javascript/main.js',
   output: {
-    filename: "main-bundled.js",
-    path: path.resolve(__dirname, "public")
+    filename: 'main-bundled.js',
+    path: path.resolve(__dirname, 'public'),
   },
-  mode: "production",
+  mode: process.env.NODE_ENV || 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  }
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  performance: {
+    hints: 'warning',
+    maxAssetSize: 2000000,
+    maxEntrypointSize: 2000000,
+  },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: './public/index.html',
+  //   }),
+  // ],
 };
