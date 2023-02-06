45ae0962ab4d206eb807bd8a9dbe9b7f18388f44
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2023-02-06 08:12:32 -0600

    updates

--- database/seed.js
+++ database/seed.js
@@ -1,21 +1,24 @@
 const { MongoClient } = require('mongodb');
 const { DB_CONNECTION_STRING } = require('../server/misc/helpers');
 const { users } = require('./seed-users');
+const User = require('../server/models/user');
 
 const seedData = async () => {
   try {
-    const client = await MongoClient.connect(DB_CONNECTION_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected successfully to MongoDB');
+    // const client = await MongoClient.connect(DB_CONNECTION_STRING, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+    // console.log('Connected successfully to MongoDB');
+    // const db = client.db();
+    // const usersCollection = db.collection('users');
 
-    const db = client.db();
-    const usersCollection = db.collection('users');
+    for (const userData of users) {
+      const user = new User(userData);
+      await user.register();
+    }
 
-    await usersCollection.insertMany(users);
-    console.log('Data seeded successfully');
-
+    console.log(`Successfully seeded ${users.length} user(s)`);
     client.close();
   } catch (error) {
     console.error('An error occurred while seeding data:', error);
